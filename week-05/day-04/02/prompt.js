8084ea60702e72f69192ecefd302e3b03c013179
L Lee <lauralee.sh@gmail.com>
2018-08-24 10:00:12 -0700

    update

--- week-05/day-04/02/prompt.js
+++ week-05/day-04/02/prompt.js
@@ -1,23 +1,32 @@
 // ======== You are given NOTHING to start with 😱 ========
 
 var fs = require ("fs");
-var search = process.argv[2];
-var dataArr = data.split(";");
+var answer = "";
+
 
 fs.readFile("quotes.txt", "utf8", function(error, data) {
+    console.log(data);
+    var dataArr = data.split(";");
+    console.log(dataArr);
+
     if (error) {
         return console.log(error);
     }
 
     else {
-        console.log(dataArr);
+        
+        for (i=0; i<dataArr.length; i++) {
+            console.log (process.argv[2]);
+            var index = dataArr[i].indexOf(process.argv[2]);
+
+            if (index>=0) {
+                answer = dataArr[i].substr(0,index-1);
+            }
+
+        }
+        console.log(answer);
     }
 
 
 });
 
-
-var obj=[];
-for (i=2; i<data.length; i++) {
-    
-}
